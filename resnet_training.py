626533d7bed9c38584f429f05e084679a9ca9bc9
Thomas Woehrle <thomas.e.woehrle@gmail.com>
2025-04-03 14:19:51 +0200

    Log losses to wandb

--- resnet_training.py
+++ resnet_training.py
@@ -1,8 +1,19 @@
+from dataclasses import asdict, dataclass
+from typing import Any
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision
 from torch.utils.data import DataLoader, Subset
+import wandb
+import wandb.wandb_run
+
+
+@dataclass
+class Config:
+    is_dry_run: bool = False
+    num_epochs: int = 10
 
 
 def get_cifar100_dataloader(is_train: bool, batch_size: int, use_every_nth: int | None = None):
@@ -17,29 +28,35 @@ def get_cifar100_dataloader(is_train: bool, batch_size: int, use_every_nth: int 
 
 
 class ResNetTraining:
-    def __init__(self, dry_run: bool = False):
+    def __init__(self, config: Config, wandb_run: (wandb.wandb_run.Run | None) = None):
         torch.manual_seed(42)
+        self.run = wandb_run
         self.model = torchvision.models.resnet18()
-        self.num_epochs = 10
+        self.num_epochs = config.num_epochs
         self.curr_ep = 0
         self.train_dataloader = get_cifar100_dataloader(
-            is_train=True, batch_size=16, use_every_nth=1000 if dry_run else None)
+            is_train=True, batch_size=16, use_every_nth=1000 if config.is_dry_run else None)
         self.val_dataloader = get_cifar100_dataloader(
-            is_train=False, batch_size=16, use_every_nth=1000 if dry_run else None)
+            is_train=False, batch_size=16, use_every_nth=1000 if config.is_dry_run else None)
         self.optimizer = optim.Adam(self.model.parameters())
         self.criterion = nn.CrossEntropyLoss()
 
     def train(self):
         """Trains for one epoch"""
         self.model.train()
-        for x, y in self.train_dataloader:
+
+        losses = torch.zeros(len(self.train_dataloader))
+        for idx, (x, y) in enumerate(self.train_dataloader):
             self.optimizer.zero_grad()
 
             y_hat = self.model(x)
             loss = self.criterion(y_hat, y)
+            losses[idx] = loss
 
             loss.backward()
             self.optimizer.step()
+
+        self.log({"train/loss": losses.mean().item()}, self.curr_ep)
 
     @torch.no_grad()
     def validate(self):
@@ -47,11 +64,12 @@ class ResNetTraining:
         self.model.eval()
 
         losses = torch.zeros(len(self.val_dataloader))
-
         for idx, (x, y) in enumerate(self.val_dataloader):
             y_hat = self.model(x)
             loss = self.criterion(y_hat, y)
             losses[idx] = loss
+
+        self.log({"val/loss": losses.mean().item()}, self.curr_ep)
 
     def run_training(self):
         for ep in range(self.num_epochs):
@@ -59,8 +77,19 @@ class ResNetTraining:
             self.train()
             self.validate()
 
+    def log(self, data: dict[str, Any], step: (int | None) = None, commit: (bool | None) = None):
+        """Effectively mirrors wandb run.log API, see https://docs.wandb.ai/ref/python/run/#log"""
+        if self.run:
+            self.run.log(data, step, commit)
+        else:
+            raise NotImplementedError()
+
 
 if __name__ == "__main__":
-    training = ResNetTraining(dry_run=True)
+    config = Config(
+        is_dry_run=True
+    )
 
-    training.run_training()
+    with wandb.init(project="thesis_baseline", group="dry_runs", config=asdict(config)) as run:
+        training = ResNetTraining(config, run)
+        training.run_training()
