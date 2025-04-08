482bb9d965383a5d327bff568fa6d362eb35ba1b
Thomas Woehrle <thomas.e.woehrle@gmail.com>
2025-04-08 13:06:10 +0200

    Add optimizer and scheduler config

--- resnet_training.py
+++ resnet_training.py
@@ -2,8 +2,8 @@ from dataclasses import asdict, dataclass
 from typing import Optional
 
 import torch
-import torch.nn as nn
 import torchvision
+import torchvision.transforms as transforms
 from torch.utils.data import DataLoader, Subset
 import wandb
 import wandb.wandb_run
@@ -13,19 +13,37 @@ import loggers
 import resnet_cifar
 import trainers
 
+# Mean and standard deviation for CIFAR-100 (precomputed)
+CIFAR100_MEAN = (0.5071, 0.4865, 0.4409)
+CIFAR100_STD = (0.2673, 0.2564, 0.2762)
 
-def get_cifar_dataloader(is_train: bool, batch_size: int, use_every_nth: int | None = None, is_cifar10: bool = False):
+
+def get_cifar_dataloader(is_train: bool, batch_size: int, num_workers: int, use_every_nth: Optional[int], is_cifar10: bool, use_img_transforms: bool):
+    train_transforms = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(CIFAR100_MEAN, CIFAR100_STD)
+    ]) if use_img_transforms else transforms.ToTensor()
+
+    val_transforms = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(CIFAR100_MEAN, CIFAR100_STD)
+    ]) if use_img_transforms else transforms.ToTensor()
+
     if is_cifar10:
         dataset = torchvision.datasets.CIFAR10(
-            "./artifacts", train=is_train, download=True, transform=torchvision.transforms.ToTensor())
+            "./artifacts", train=is_train, download=True, transform=train_transforms if is_train else val_transforms)
     else:
         dataset = torchvision.datasets.CIFAR100(
-            "./artifacts", train=is_train, download=True, transform=torchvision.transforms.ToTensor())
+            "./artifacts", train=is_train, download=True, transform=train_transforms if is_train else val_transforms)
+
     if use_every_nth is not None:
         dataset = Subset(dataset, indices=range(
             0, len(dataset), use_every_nth))
 
-    dataloader = DataLoader(dataset, batch_size, shuffle=is_train)
+    dataloader = DataLoader(dataset, batch_size,
+                            num_workers=num_workers, shuffle=is_train)
     return dataloader
 
 
@@ -35,7 +53,9 @@ class TrainingConfig:
     evaluator_config: evaluators.Evaluator1Config
     num_epochs: int
     batch_size: int
-    do_use_data_subset: bool = False
+    use_img_transforms: bool
+    num_workers: int = 0
+    use_data_subset: bool = False
     is_cifar10: bool = False
 
 
@@ -43,13 +63,17 @@ def run_training(training_config: TrainingConfig, wandb_run: Optional[wandb.wand
     train_dataloader = get_cifar_dataloader(
         is_train=True,
         batch_size=training_config.batch_size,
-        use_every_nth=1000 if training_config.do_use_data_subset else None,
-        is_cifar10=training_config.is_cifar10)
+        use_every_nth=1000 if training_config.use_data_subset else None,
+        is_cifar10=training_config.is_cifar10,
+        use_img_transforms=training_config.use_img_transforms,
+        num_workers=training_config.num_workers)
     val_dataloader = get_cifar_dataloader(
         is_train=False,
         batch_size=training_config.batch_size,
-        use_every_nth=1000 if training_config.do_use_data_subset else None,
-        is_cifar10=training_config.is_cifar10)
+        use_every_nth=1000 if training_config.use_data_subset else None,
+        is_cifar10=training_config.is_cifar10,
+        use_img_transforms=training_config.use_img_transforms,
+        num_workers=training_config.num_workers)
 
     model = resnet_cifar.ResNet18(
         nb_cls=10 if training_config.is_cifar10 else 100)
@@ -69,9 +93,21 @@ if __name__ == "__main__":
     device = torch.device("mps")
     is_test_run = False
     is_cifar10 = False
+    num_epochs = 30
 
     trainer_config = trainers.NormalTrainerConfig(
-        device=device
+        device=device,
+        optimizer_config=trainers.OptimizerConfig(
+            optimizer_name="sgd",
+            lr=0.1,
+            weight_decay=1e-4,
+            momentum=0.9
+        ),
+        lr_scheduler_config=trainers.LRSchedulerConfig(
+            use_cos_annealing_lr=True,
+            T_max=num_epochs,
+            eta_min=0
+        )
     )
     evaluator_config = evaluators.Evaluator1Config(
         device=device,
@@ -81,10 +117,12 @@ if __name__ == "__main__":
     config = TrainingConfig(
         trainer_config=trainer_config,
         evaluator_config=evaluator_config,
-        num_epochs=20,
+        num_epochs=num_epochs,
         batch_size=16,
-        do_use_data_subset=True if is_test_run else False,
-        is_cifar10=is_cifar10
+        use_img_transforms=True,
+        use_data_subset=True if is_test_run else False,
+        is_cifar10=is_cifar10,
+        num_workers=0 if is_test_run else 4
     )
 
     project = "thesis_baseline_testruns" if is_test_run else "thesis_baseline"
--- trainers.py
+++ trainers.py
@@ -1,24 +1,65 @@
 from dataclasses import dataclass
+from typing import Optional
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader
 import tqdm
+from torch.utils.data import DataLoader
+from torch.optim.optimizer import Optimizer
+from torch.optim.lr_scheduler import LRScheduler
 
 import loggers
 
 
 @dataclass
+class OptimizerConfig:
+    optimizer_name: str
+    lr: float
+    weight_decay: float = 0.0
+    momentum: float = 0.0
+
+
+@dataclass
+class LRSchedulerConfig:
+    use_cos_annealing_lr: bool
+    T_max: Optional[int] = None
+    eta_min: Optional[float] = None
+
+
+@dataclass
 class NormalTrainerConfig:
     device: torch.device
+    optimizer_config: OptimizerConfig
+    lr_scheduler_config: LRSchedulerConfig
+
+
+def get_optimizer(model: nn.Module, optimizer_config: OptimizerConfig) -> Optimizer:
+    if optimizer_config.optimizer_name == "sgd":
+        return optim.SGD(model.parameters(), lr=optimizer_config.lr, momentum=optimizer_config.momentum, weight_decay=optimizer_config.weight_decay)
+    elif optimizer_config.optimizer_name == "adam":
+        return optim.Adam(model.parameters(), lr=optimizer_config.lr, weight_decay=optimizer_config.weight_decay)
+    else:
+        raise ValueError(
+            f"Optimizer {optimizer_config.optimizer_name} is not supported")
+
+
+def get_lr_scheduler(optimizer: Optimizer, scheduler_config: LRSchedulerConfig) -> Optional[LRScheduler]:
+    if scheduler_config.use_cos_annealing_lr:
+        assert scheduler_config.eta_min is not None
+        assert scheduler_config.T_max is not None
+        return optim.lr_scheduler.CosineAnnealingLR(optimizer, scheduler_config.T_max, scheduler_config.eta_min)
+    else:
+        return None
 
 
 class NormalTrainer:
     def __init__(self, model: nn.Module, dataloader: DataLoader, config: NormalTrainerConfig, logger: loggers.Logger):
         self.model = model
         self.dataloader = dataloader
-        self.optimizer = optim.Adam(model.parameters())
+        self.optimizer = get_optimizer(model, config.optimizer_config)
+        self.lr_scheduler = get_lr_scheduler(
+            self.optimizer, config.lr_scheduler_config)
         self.criterion = nn.CrossEntropyLoss()
 
         self.config = config
@@ -47,3 +88,8 @@ class NormalTrainer:
             losses[batch_idx] = self.train_batch(batch, batch_idx)
 
         self.logger.log({"train/loss": losses.mean().item()}, epoch)
+
+        if self.lr_scheduler:
+            self.logger.log(
+                {"debug/lr": self.lr_scheduler.get_last_lr()[0]}, epoch)
+            self.lr_scheduler.step()
