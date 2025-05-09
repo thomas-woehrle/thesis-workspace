bfebaa54909ab188da42caf9cc684d7c527d446c
Thomas Woehrle <thomas.e.woehrle@gmail.com>
2025-05-09 12:43:41 +0200

    Adjust backprop trainer to new structure and integrate into config_manager.py

--- trainers.py
+++ trainers.py
@@ -1,39 +1,36 @@
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
-from typing import Optional
+from typing import Callable, Optional
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
 import tqdm
-from torch._functorch.apis import vmap
 from torch.utils.data import DataLoader
 
 import loggers
-import optimizers
 
 
-@dataclass
-class TrainerConfig:
-    device: torch.device
-    dtype: torch.dtype
-    bn_track_running_stats: bool
-
-
-class Trainer[ConfigType: TrainerConfig](ABC):
+class Trainer(ABC):
     def __init__(
         self,
         model: nn.Module,
         dataloader: DataLoader,
+        optimizer: optim.Optimizer,
+        lr_scheduler: Optional[optim.lr_scheduler.LRScheduler],
+        criterion: Callable[[torch.Tensor, torch.Tensor], torch.Tensor],
+        device: torch.device,
+        dtype: torch.dtype,
         logger: loggers.Logger,
-        config: ConfigType,
     ):
         self.model = model
-        self.model.to(device=config.device, dtype=config.dtype)
+        self.model.to(device=device, dtype=dtype)
         self.dataloader = dataloader
+        self.optimizer = optimizer
+        self.lr_scheduler = lr_scheduler
+        self.criterion = criterion
+        self.device = device
+        self.dtype = dtype
         self.logger = logger
-        self.config = config
 
     @abstractmethod
     def train_step(self, x: torch.Tensor, y: torch.Tensor, batch_idx: int) -> torch.Tensor | float:
@@ -43,7 +40,7 @@ class Trainer[ConfigType: TrainerConfig](ABC):
 
     def train_epoch(self, epoch: int):
         self.model.train()
-        self.model.to(device=self.config.device, dtype=self.config.dtype)
+        self.model.to(device=self.device, dtype=self.dtype)
 
         losses = torch.zeros(len(self.dataloader))
         for batch_idx, batch in enumerate(
@@ -52,104 +49,15 @@ class Trainer[ConfigType: TrainerConfig](ABC):
             x, y = batch
             # x should be desired dtype, y should be long
             x, y = (
-                x.to(device=self.config.device, dtype=self.config.dtype, non_blocking=True),
-                y.to(device=self.config.device, dtype=torch.long, non_blocking=True),
+                x.to(device=self.device, dtype=self.dtype, non_blocking=True),
+                y.to(device=self.device, dtype=torch.long, non_blocking=True),
             )
             losses[batch_idx] = self.train_step(x, y, batch_idx)
 
         self.logger.log({"train/loss": losses.mean().item()}, epoch)
 
 
-@dataclass
-class OptimizerConfig:
-    optimizer_slug: str
-    lr: float
-    weight_decay: float = 0.0
-    momentum: float = 0.0
-
-
-@dataclass
-class LRSchedulerConfig:
-    lr_scheduler_slug: Optional[str]
-    lr_scheduler_slug: Optional[str]
-    T_max: Optional[int] = None
-    eta_min: Optional[float] = None
-    milestones: Optional[list[int]] = None
-    gamma: Optional[float] = None
-    milestones: Optional[list[int]] = None
-    gamma: Optional[float] = None
-
-
-@dataclass
-class NormalTrainerConfig(TrainerConfig):
-    optimizer_config: OptimizerConfig
-    lr_scheduler_config: LRSchedulerConfig
-
-
-def get_optimizer(model: nn.Module, optimizer_config: OptimizerConfig) -> optim.Optimizer:
-    if optimizer_config.optimizer_slug == "sgd":
-        return optim.SGD(
-            model.parameters(),
-            lr=optimizer_config.lr,
-            momentum=optimizer_config.momentum,
-            weight_decay=optimizer_config.weight_decay,
-        )
-    elif optimizer_config.optimizer_slug == "adam":
-        return optim.Adam(
-            model.parameters(),
-            lr=optimizer_config.lr,
-            weight_decay=optimizer_config.weight_decay,
-        )
-    elif optimizer_config.optimizer_slug == "adamw":
-        return optim.AdamW(
-            model.parameters(),
-            lr=optimizer_config.lr,
-            weight_decay=optimizer_config.weight_decay,
-        )
-    else:
-        raise ValueError(f"Optimizer {optimizer_config.optimizer_slug} is not supported")
-
-
-def get_lr_scheduler(
-    optimizer: optim.Optimizer, scheduler_config: LRSchedulerConfig
-) -> Optional[optim.lr_scheduler.LRScheduler]:
-    if scheduler_config.lr_scheduler_slug is None:
-        return None
-    elif scheduler_config.lr_scheduler_slug == "cosine_annealing":
-        assert scheduler_config.eta_min is not None
-        assert scheduler_config.T_max is not None
-        return optim.lr_scheduler.CosineAnnealingLR(
-            optimizer, scheduler_config.T_max, scheduler_config.eta_min
-        )
-    elif scheduler_config.lr_scheduler_slug == "multi_step":
-        assert scheduler_config.milestones is not None
-        assert scheduler_config.gamma is not None
-        return optim.lr_scheduler.MultiStepLR(
-            optimizer, scheduler_config.milestones, scheduler_config.gamma
-        )
-    elif scheduler_config.lr_scheduler_slug == "multi_step":
-        assert scheduler_config.milestones is not None
-        assert scheduler_config.gamma is not None
-        return optim.lr_scheduler.MultiStepLR(
-            optimizer, scheduler_config.milestones, scheduler_config.gamma
-        )
-    else:
-        raise ValueError(f"LR scheduler {scheduler_config.lr_scheduler_slug} is not supported")
-
-
-class NormalTrainer(Trainer[NormalTrainerConfig]):
-    def __init__(
-        self,
-        model: nn.Module,
-        dataloader: DataLoader,
-        logger: loggers.Logger,
-        config: NormalTrainerConfig,
-    ):
-        super().__init__(model, dataloader, logger, config)
-        self.optimizer = get_optimizer(model, config.optimizer_config)
-        self.lr_scheduler = get_lr_scheduler(self.optimizer, config.lr_scheduler_config)
-        self.criterion = nn.CrossEntropyLoss()
-
+class NormalTrainer(Trainer):
     def train_step(self, x: torch.Tensor, y: torch.Tensor, batch_idx: int) -> torch.Tensor | float:
         self.optimizer.zero_grad()
 
@@ -169,106 +77,111 @@ class NormalTrainer(Trainer[NormalTrainerConfig]):
             self.lr_scheduler.step()
 
 
-@dataclass
-class OpenAIEvolutionaryTrainerConfig(TrainerConfig):
-    popsize: int
-    sigma: float
-    lr: float
-    use_antithetic_sampling: bool
-    use_parallel_forward_pass: bool
+# @dataclass
+# class OpenAIEvolutionaryTrainerConfig(TrainerConfig):
+#     popsize: int
+#     sigma: float
+#     lr: float
+#     use_antithetic_sampling: bool
+#     use_parallel_forward_pass: bool
 
 
-class OpenAIEvolutionaryTrainer(Trainer[OpenAIEvolutionaryTrainerConfig]):
-    def __init__(
-        self,
-        model: nn.Module,
-        dataloader: DataLoader,
-        logger: loggers.Logger,
-        config: OpenAIEvolutionaryTrainerConfig,
-    ):
-        super().__init__(model, dataloader, logger, config)
-        self.optimizer = optimizers.OpenAIEvolutionaryOptimizer(
-            config.popsize,
-            config.sigma,
-            config.lr,
-            self.model,
-            config.use_antithetic_sampling,
-            config.device,
-            config.dtype,
-        )
-        self.criterion = nn.CrossEntropyLoss()
-        self.multi_criterion = vmap(F.cross_entropy, in_dims=(0, None))
+# class OpenAIEvolutionaryTrainer(Trainer[OpenAIEvolutionaryTrainerConfig]):
+#     def __init__(
+#         self,
+#         model: nn.Module,
+#         dataloader: DataLoader,
+#         logger: loggers.Logger,
+#         config: OpenAIEvolutionaryTrainerConfig,
+#             popsize: int,
+#         sigma: float,
+#         lr: float,
+#         use_antithetic_sampling: bool,
+#         use_parallel_forward_pass: bool
+#     ):
+#         super().__init__(model, dataloader, logger, config)
+#         self.optimizer = optimizers.OpenAIEvolutionaryOptimizer(
+#             config.popsize,
+#             config.sigma,
+#             config.lr,
+#             self.model,
+#             config.use_antithetic_sampling,
+#             config.device,
+#             config.dtype,
+#         )
+#         self.criterion = nn.CrossEntropyLoss()
+#         self.multi_criterion = vmap(F.cross_entropy, in_dims=(0, None))
 
-    def train_step(self, x: torch.Tensor, y: torch.Tensor, batch_idx: int) -> torch.Tensor | float:
-        self.optimizer.prepare_mutations()
+#     def train_step(self, x: torch.Tensor, y: torch.Tensor, batch_idx: int) -> torch.Tensor | float:
+#         self.optimizer.prepare_mutations()
 
-        if self.config.use_parallel_forward_pass:
-            y_hat = self.optimizer.parallel_forward_pass(x)
+#         if self.config.use_parallel_forward_pass:
+#             y_hat = self.optimizer.parallel_forward_pass(x)
 
-            losses = self.multi_criterion(y_hat, y)
-        else:
-            losses = torch.zeros(self.config.popsize, device=self.config.device)
+#             losses = self.multi_criterion(y_hat, y)
+#         else:
+#             losses = torch.zeros(self.config.popsize, device=self.config.device)
 
-            for i in range(self.config.popsize):
-                self.optimizer.load_mutation_into_model(i)
-                y_hat = self.model(x)
-                losses[i] = self.criterion(y_hat, y)
+#             for i in range(self.config.popsize):
+#                 self.optimizer.load_mutation_into_model(i)
+#                 y_hat = self.model(x)
+#                 losses[i] = self.criterion(y_hat, y)
 
-        self.optimizer.step(losses)
-        return losses.mean()
+#         self.optimizer.step(losses)
+#         return losses.mean()
 
-    @torch.no_grad()
-    def train_epoch(self, epoch: int):
-        super().train_epoch(epoch)
+#     @torch.no_grad()
+#     def train_epoch(self, epoch: int):
+#         super().train_epoch(epoch)
 
-        if self.config.use_parallel_forward_pass and self.config.bn_track_running_stats:
-            # load buffers into model, only necessary to do this manually in the case of parallel pass
-            named_buffers = self.optimizer.get_current_buffers()
-            self.model.load_state_dict(named_buffers, strict=False)
-
-
-@dataclass
-class SimpleEvolutionaryTrainerConfig(TrainerConfig):
-    n_families: int
-    members_per_family: int
-    sigma: float
+#         if self.config.use_parallel_forward_pass and self.config.bn_track_running_stats:
+#             # load buffers into model, only necessary to do this manually in the case of parallel pass
+#             named_buffers = self.optimizer.get_current_buffers()
+#             self.model.load_state_dict(named_buffers, strict=False)
 
 
-class SimpleEvolutionaryTrainer(Trainer[SimpleEvolutionaryTrainerConfig]):
-    # init as super()...
+# @dataclass
+# class SimpleEvolutionaryTrainerConfig(TrainerConfig):
+#     n_families: int
+#     members_per_family: int
+#     sigma: float
 
-    def __init__(
-        self,
-        model: nn.Module,
-        dataloader: DataLoader,
-        logger: loggers.Logger,
-        config: SimpleEvolutionaryTrainerConfig,
-    ):
-        super().__init__(model, dataloader, logger, config)
-        self.optimizer = optimizers.SimpleEvolutionaryOptimizer(
-            config.n_families,
-            config.members_per_family,
-            config.sigma,
-            self.model,
-            config.device,
-            config.dtype,
-        )
-        self.criterion = nn.CrossEntropyLoss()
 
-    def train_step(self, x: torch.Tensor, y: torch.Tensor, batch_idx: int) -> torch.Tensor | float:
-        self.optimizer.mutate()
+# class SimpleEvolutionaryTrainer(Trainer[SimpleEvolutionaryTrainerConfig]):
+#     # init as super()...
 
-        losses = torch.zeros(self.optimizer.n_families, self.optimizer.members_per_family)
-        for family_idx in range(self.optimizer.n_families):
-            for member_idx in range(self.optimizer.members_per_family):
-                self.optimizer.load_individual_into_model(family_idx, member_idx)
-                y_hat = self.model(x)
-                losses[family_idx][member_idx] = self.criterion(y_hat, y)
+#     def __init__(
+#         self,
+#         model: nn.Module,
+#         dataloader: DataLoader,
+#         logger: loggers.Logger,
+#         config: SimpleEvolutionaryTrainerConfig,
+#     ):
+#         super().__init__(model, dataloader, logger, config)
+#         self.optimizer = optimizers.SimpleEvolutionaryOptimizer(
+#             config.n_families,
+#             config.members_per_family,
+#             config.sigma,
+#             self.model,
+#             config.device,
+#             config.dtype,
+#         )
+#         self.criterion = nn.CrossEntropyLoss()
 
-        self.optimizer.step(losses)
+#     def train_step(self, x: torch.Tensor, y: torch.Tensor, batch_idx: int) -> torch.Tensor | float:
+#         self.optimizer.mutate()
 
-        return losses.mean()
+#         losses = torch.zeros(self.optimizer.n_families, self.optimizer.members_per_family)
+#         for family_idx in range(self.optimizer.n_families):
+#             for member_idx in range(self.optimizer.members_per_family):
+#                 self.optimizer.load_individual_into_model(family_idx, member_idx)
+#                 y_hat = self.model(x)
+#                 losses[family_idx][member_idx] = self.criterion(y_hat, y)
 
-    @torch.no_grad()
-    def train_epoch(self, epoch: int):
-        return super().train_epoch(epoch)
+#         self.optimizer.step(losses)
+
+#         return losses.mean()
+
+#     @torch.no_grad()
+#     def train_epoch(self, epoch: int):
+#         return super().train_epoch(epoch)
