c5b871dda9a7bd49aa2167f953c122edf5a0076c
Thomas Woehrle <thomas.e.woehrle@gmail.com>
2025-05-12 14:22:52 +0200

    Remove TrainerConfig

--- utils.py
+++ utils.py
@@ -26,7 +26,6 @@ def load_config_from_file(file_path: str) -> "config.RunConfig":
     data_config = config.DataConfig(**config_dict.get("DATA_CONFIG", {}))
     optimizer_config = config.OptimizerConfig(**config_dict.get("OPTIMIZER_CONFIG", {}))
     lr_scheduler_config = config.LRSchedulerConfig(**config_dict.get("LR_SCHEDULER_CONFIG", {}))
-    trainer_config = config.TrainerConfig(**config_dict.get("TRAINER_CONFIG", {}))
     evaluator_config = config.EvaluatorConfig(**config_dict.get("EVALUATOR_CONFIG", {}))
 
     return config.RunConfig(
@@ -35,7 +34,6 @@ def load_config_from_file(file_path: str) -> "config.RunConfig":
         data_config=data_config,
         optimizer_config=optimizer_config,
         lr_scheduler_config=lr_scheduler_config,
-        trainer_config=trainer_config,
         evaluator_config=evaluator_config,
     )
 
