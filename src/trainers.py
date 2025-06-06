8f07a34cee262b192fdc21b6c7fe874f3d1d8d53
Thomas Woehrle <thomas.e.woehrle@gmail.com>
2025-06-06 12:56:44 +0200

    Integrate mixed-precision

--- src/trainers.py
+++ src/trainers.py
@@ -21,6 +21,7 @@ class Trainer(ABC):
         optimizer: optim.Optimizer,
         lr_scheduler: Optional[optim.lr_scheduler.LRScheduler],
         criterion: Callable[[torch.Tensor, torch.Tensor], torch.Tensor],
+        mp_dtype: torch.dtype,
         logger: loggers.Logger,
     ):
         self.model = model
@@ -29,6 +30,7 @@ class Trainer(ABC):
         self.optimizer = optimizer
         self.lr_scheduler = lr_scheduler
         self.criterion = criterion
+        self.mp_dtype = mp_dtype
         self.logger = logger
 
     @abstractmethod
@@ -47,16 +49,12 @@ class Trainer(ABC):
             desc=f"Training - Steps {start_train_step} - {start_train_step + num_steps - 1}",
         ):
             x, y = next(self.data_iterator)
-            # x should be desired dtype, y should be long
             x, y = (
                 x.to(
                     device=next(self.model.parameters()).device,
-                    dtype=next(self.model.parameters()).dtype,
                     non_blocking=True,
                 ),
-                y.to(
-                    device=next(self.model.parameters()).device, dtype=torch.long, non_blocking=True
-                ),
+                y.to(device=next(self.model.parameters()).device, non_blocking=True),
             )
             losses[i] = self.train_step(x, y)
 
@@ -104,6 +102,7 @@ class EvolutionaryTrainer(Trainer):
         use_parallel_forward_pass: bool,
         bn_track_running_stats: bool,
         use_instance_norm: bool,
+        mp_dtype: torch.dtype,
         logger: loggers.Logger,
         use_torch_compile: bool,
     ):
@@ -113,6 +112,7 @@ class EvolutionaryTrainer(Trainer):
             optimizer=optimizer,
             lr_scheduler=lr_scheduler,
             criterion=criterion,
+            mp_dtype=mp_dtype,
             logger=logger,
         )
         self.use_parallel_forward_pass = use_parallel_forward_pass
@@ -125,7 +125,9 @@ class EvolutionaryTrainer(Trainer):
         self.batched_criterion = vmap(self.criterion, in_dims=(0, None))
         self.popsize = self.optimizer.popsize
         self.batched_named_buffers = {
-            n: torch.stack([b] * self.popsize, dim=0) for n, b in self.model.named_buffers()
+            # manually moved to the correct dtype, because autocast doesn't realiably do it in this case
+            n: torch.stack([b] * self.popsize, dim=0).to(dtype=self.mp_dtype)
+            for n, b in self.model.named_buffers()
         }
 
     def _get_batched_named_params(self, batched_flat_params: torch.Tensor, popsize: int):
@@ -150,25 +152,30 @@ class EvolutionaryTrainer(Trainer):
 
         popsize = mutations.shape[0]
 
-        if self.use_parallel_forward_pass:
-            y_hat = self.parallel_forward_pass_fn(
-                (mutated_batched_named_params, self.batched_named_buffers), x
-            )
-            losses = self.batched_criterion(y_hat, y)
-
-        else:
-            losses = torch.zeros(
-                popsize,
-                device=mutated_batched_flat_params.device,
-                dtype=mutated_batched_flat_params.dtype,
-            )
-
-            for i in range(popsize):
-                nn.utils.vector_to_parameters(
-                    mutated_batched_flat_params[i], self.model.parameters()
+        with torch.autocast(
+            device_type=next(self.model.parameters()).device.type,
+            dtype=self.mp_dtype,
+        ):
+            if self.use_parallel_forward_pass:
+                y_hat = self.parallel_forward_pass_fn(
+                    (mutated_batched_named_params, self.batched_named_buffers), x
                 )
-                y_hat = self.model(x)
-                losses[i] = self.criterion(y_hat, y)
+                # Convert to full precision for loss computation to ensure numerical stability
+                losses = self.batched_criterion(y_hat.float(), y)
+
+            else:
+                losses = torch.zeros(
+                    popsize,
+                    device=mutated_batched_flat_params.device,
+                )
+
+                for i in range(popsize):
+                    nn.utils.vector_to_parameters(
+                        mutated_batched_flat_params[i], self.model.parameters()
+                    )
+                    y_hat = self.model(x)
+                    # Convert to full precision for loss computation to ensure numerical stability
+                    losses[i] = self.criterion(y_hat.float(), y)
 
         self.optimizer.step(losses, mutations)
         return losses.mean()
